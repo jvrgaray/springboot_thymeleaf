3bfac1fba3c9f4f93929d383c8e2eb5ae1c992c8
Javier Garay <javier.garay@2brains.cl>
2020-08-19 16:01:07 -0400

    29. Anotación @RequestMapping a nivel de un método - Práctica

---
+++ src/main/java/com/jgaray/controller/CategoriasController.java
@@ -0,0 +1,24 @@
+package com.jgaray.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CategoriasController {
+	@GetMapping("/index")
+	public String mostrarIndex(Model model) {
+		return "categorias/listarCategorias";
+	}
+	
+	@GetMapping("/create")
+	public String crear() {
+		return "categorias/formCategoria";
+	}
+	
+	@PostMapping("/save")
+	public String guardar() {
+		return "categorias/listCategorias";
+	}
+}
