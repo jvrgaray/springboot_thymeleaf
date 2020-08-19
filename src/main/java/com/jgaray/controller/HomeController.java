0bdba31d41a3b7efc31b556208f73047892d1a0b
Javier Garay <javier.garay@2brains.cl>
2020-08-19 13:37:59 -0400

    20. Agregar tipo de dato List<Vacante> al modelo y desplegarlos en una tabla HTML.

--- src/main/java/com/jgaray/controller/HomeController.java
+++ src/main/java/com/jgaray/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.jgaray.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,6 +14,13 @@ import com.jgaray.model.Vacante;
 
 @Controller
 public class HomeController {
+	@GetMapping("/tabla")
+	public String motrarTabla(Model model) {
+		List<Vacante> lista = getVacantes();
+		model.addAttribute("vacantes", lista);
+		return "tabla";
+	}
+	
 	@GetMapping("/detalle")
 	public String mostrarDetalle(Model model) {
 		Vacante vacante = new Vacante();
@@ -54,5 +63,48 @@ public class HomeController {
 
 		
 		return "home";
+	}
+	
+	private List<Vacante> getVacantes(){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		List<Vacante> lista = new  LinkedList<Vacante>();
+		
+		try {
+			Vacante v1 = new Vacante();
+			v1.setId(1);
+			v1.setNombre("vacante 1");
+			v1.setDescripcion("descripcion 1");
+			v1.setFecha(simpleDateFormat.parse("01-01-2020"));
+			v1.setSalario(1000.0);
+			
+			Vacante v2 = new Vacante();
+			v2.setId(2);
+			v2.setNombre("vacante 2");
+			v2.setDescripcion("descripcion 2");
+			v2.setFecha(simpleDateFormat.parse("02-02-2020"));
+			v2.setSalario(2000.0);
+			
+			Vacante v3 = new Vacante();
+			v3.setId(3);
+			v3.setNombre("vacante 3");
+			v3.setDescripcion("descripcion 3");
+			v3.setFecha(simpleDateFormat.parse("03-03-2020"));
+			v3.setSalario(3000.0);
+			
+			Vacante v4 = new Vacante();
+			v4.setId(4);
+			v4.setNombre("vacante 4");
+			v4.setDescripcion("descripcion 4");
+			v4.setFecha(simpleDateFormat.parse("04-04-2020"));
+			v4.setSalario(4000.0);
+			
+			lista.add(v1);
+			lista.add(v2);
+			lista.add(v3);
+			lista.add(v4);
+		} catch(ParseException e) {
+			System.out.println("Error: "+e.getMessage());
+		}
+		return lista;
 	}
 }
