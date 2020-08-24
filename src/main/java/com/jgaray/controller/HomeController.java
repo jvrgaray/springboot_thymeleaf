a4fb98e7cff137cd6a57ad6b9dd4a35019188dce
Javier Garay <javier.garay@2brains.cl>
2020-08-24 16:05:30 -0400

    43. Integrar el diseño de la página principal dinámicamente (Parte 2)

--- src/main/java/com/jgaray/controller/HomeController.java
+++ src/main/java/com/jgaray/controller/HomeController.java
@@ -53,20 +53,8 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
-//		model.addAttribute("mensaje", "Bienvenidos a empleos App");
-//		model.addAttribute("fecha", new Date());
-		
-		String nombre = "Auxiliar de Contabilidad";
-		Date fechaPub = new Date();
-		double salario = 9000.0;
-		boolean vigente = true;
-		
-		model.addAttribute("nombre", nombre);
-		model.addAttribute("fechaPub", fechaPub);
-		model.addAttribute("salario", salario);
-		model.addAttribute("vigente", vigente);
-
-		
+		List<Vacante> lista = serviceVacantes.buscarTodas();
+		model.addAttribute("vacantes", lista);
 		return "home";
 	}
 	
